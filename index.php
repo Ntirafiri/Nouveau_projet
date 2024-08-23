4641c30409f509961141231b7204f4587c204af9
Ntibitangira Firiberi <firiberi@gmail.com>
2024-08-23 11:50:29 +0200

    nieme essai

---
+++ index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Page de contact</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+</head>
+
+<body class="container-fluid">
+
+  <?php
+
+  include("include/header_root.php");
+
+  ?>
+
+  <!-- Debut partie centrale -->
+  <!-- Debut section 1: Carousel- -->
+  <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner h-75">
+      <div class="carousel-item active">
+        <img src="img/maison1.jpeg" class="d-block p-5" style="width: 100%; height: 45em; object-fit: fill" alt="..." />
+      </div>
+      <div class="carousel-item">
+        <img src="img/maison2.jpeg" class="d-block p-5" style="width: 100%; height: 45em; object-fit: fill" alt="..." />
+      </div>
+      <div class="carousel-item">
+        <img src="img/maison3.jpeg" class="d-block p-5" style="width: 100%; height: 45em; object-fit: fill" alt="..." />
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+  <!-- Fin section 1: Carousel -->
+
+  <!-- Début section 2 -->
+  <div class="container">
+    <div class="row">
+      <h1 style="text-align: center">AGENCE IMMOBILIERE IMMOCOGE</h1>
+      <div style="text-align: justify">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+        quasi, ipsam quo tenetur consequuntur nulla harum iste consequatur
+        aspernatur voluptate cumque, nisi nesciunt ipsum! Earum aut beatae
+        dignissimos reprehenderit corrupti. Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Distinctio, vero odio. Soluta maxime
+        facilis deleniti veniam, animi accusamus ipsum. Exercitationem
+        dolorum similique quibusdam! Earum repudiandae incidunt ab, facilis
+        hic porro officiis sed iste esse commodi debitis deleniti fugit
+        nulla dignissimos necessitatibus architecto ex, eos dicta. Maxime
+        eveniet, exercitationem ipsum deserunt eos officiis veniam culpa.
+        Rem ut iure officia eos corporis optio esse suscipit, numquam hic
+        velit dolor obcaecati odio quibusdam! Nesciunt voluptatibus ad
+        officia, doloremque itaque tempore voluptate labore adipisci modi,
+        vitae maiores consectetur libero a laborum alias repellat aspernatur
+        illum at perspiciatis, rerum animi aliquam id. Laudantium,
+        doloremque deserunt?
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid d-flex mt-5">
+    <div class="container">
+      <div class="row g-3 col-lg-12">
+        <div class="col col-lg-4">
+          <div class="card">
+            <img src="img/maison4.jpeg" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">Villa n°1</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make
+                up the bulk of the card's content.
+              </p>
+              <a href="#" class="btn btn-primary">Plus de détails...</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col col-lg-4">
+          <div class="card">
+            <img src="img/maison5.jpeg" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">Villa n°2</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make
+                up the bulk of the card's content.
+              </p>
+              <a href="#" class="btn btn-primary">Plus de détails...</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col col col-lg-4">
+          <div class="card">
+            <img src="img/maison1.jpeg" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">Villa n°3</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make
+                up the bulk of the card's content.
+              </p>
+              <a href="#" class="btn btn-primary">Plus de détails...</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col col col-lg-4">
+          <div class="card">
+            <img src="img/maison2.jpeg" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">Villa n°4</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make
+                up the bulk of the card's content.
+              </p>
+              <a href="#" class="btn btn-primary">Plus de détails...</a>
+            </div>
+          </div>
+        </div>
+        <div class="col col col-lg-4">
+          <div class="card">
+            <img src="img/maison2.jpeg" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">Villa n°5</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make
+                up the bulk of the card's content.
+              </p>
+              <a href="#" class="btn btn-primary">Plus de détails...</a>
+            </div>
+          </div>
+        </div>
+        <div class="col col col-lg-4">
+          <div class="card">
+            <img src="img/maison2.jpeg" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">Villa n°6</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make
+                up the bulk of the card's content.
+              </p>
+              <a href="#" class="btn btn-primary">Plus de détails...</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container end-0 col-lg-2">
+      <div class="col">
+        <h3 class="mb-3">Liens utiles</h3>
+        <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="#">
+          Affichage par quartier
+        </a>
+        <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="#">
+          Affichage par catégorie
+        </a>
+      </div>
+    </div>
+  </div>
+  <!-- Fin section 2 -->
+  <!-- Fin partie principale -->
+
+  <div class="mt-5">
+    <?php include("include/footer_root.php"); ?>
+  </div>
+  </div>
+  <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
